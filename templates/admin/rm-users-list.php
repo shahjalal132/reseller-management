391356fd699adf6a7416a4be0a7222d2da1d57b2
coderjahidul <sobuz0349@gmail.com>
2026-04-05 23:03:00 +0600

    add: action buttons on admin users list

--- templates/admin/rm-users-list.php
+++ templates/admin/rm-users-list.php
@@ -73,9 +73,11 @@ $rm_search = sanitize_text_field( wp_unslash( $_GET['rm_search'] ?? '' ) ); // p
                 $status        = (string) $rm_u['status'];
                 $status_label  = 'approved' === $status ? __( 'active', 'reseller-management' ) : ucfirst( $status );
 
-                $view_url   = admin_url( 'admin.php?page=reseller-hub-user-view&reseller_id=' . $uid );
-                $edit_url   = admin_url( 'user-edit.php?user_id=' . $uid );
-                $delete_url = wp_nonce_url(
+                $view_url       = admin_url( 'admin.php?page=reseller-hub-user-view&reseller_id=' . $uid );
+                $edit_url       = admin_url( 'user-edit.php?user_id=' . $uid );
+                $orders_url     = admin_url( 'admin.php?page=reseller-hub-user-orders&reseller_id=' . $uid );
+                $statements_url = admin_url( 'admin.php?page=reseller-hub-user-statements&reseller_id=' . $uid );
+                $delete_url     = wp_nonce_url(
                     admin_url( 'admin-post.php?action=rm_delete_reseller&reseller_id=' . $uid ),
                     'rm_delete_reseller_' . $uid
                 );
@@ -110,6 +112,22 @@ $rm_search = sanitize_text_field( wp_unslash( $_GET['rm_search'] ?? '' ) ); // p
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"/>
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                            </svg>
+                        </a>
+                        <!-- View Orders -->
+                        <a href="<?php echo esc_url( $orders_url ); ?>"
+                           class="rm-action-btn orders"
+                           title="<?php esc_attr_e( 'View Orders', 'reseller-management' ); ?>">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                            </svg>
+                        </a>
+                        <!-- View Statements -->
+                        <a href="<?php echo esc_url( $statements_url ); ?>"
+                           class="rm-action-btn statements"
+                           title="<?php esc_attr_e( 'View Statements', 'reseller-management' ); ?>">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                             </svg>
                         </a>
                         <!-- Edit -->
