0f01509cad2ede3e66483ce664a657b8440b5ed6
coderjahidul <sobuz0349@gmail.com>
2026-04-05 23:28:56 +0600

    modify: admin dashboard withdrawls page

--- templates/admin/rm-withdrawals.php
+++ templates/admin/rm-withdrawals.php
@@ -441,9 +441,9 @@ jQuery(document).ready(function ($) {
             html += '</td>';
 
             html += '<td><div class="rm-wd-acts">';
-            html +=   '<a href="' + row.view_url + '" class="rm-wd-btn rm-wd-btn-view">View</a>';
-            html +=   '<a href="' + row.edit_url + '" class="rm-wd-btn rm-wd-btn-edit is-edit">Edit</a>';
-            html +=   '<a href="' + row.delete_url + '" class="rm-wd-btn rm-wd-btn-del" onclick="return confirm(\'Delete this withdrawal?\');">Del</a>';
+            html +=   '<a href="' + row.view_url + '" class="rm-wd-btn rm-wd-btn-view" title="View"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg></a>';
+            html +=   '<a href="' + row.edit_url + '" class="rm-wd-btn rm-wd-btn-edit is-edit" title="Edit"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg></a>';
+            html +=   '<a href="' + row.delete_url + '" class="rm-wd-btn rm-wd-btn-del" onclick="return confirm(\'Delete this withdrawal?\');" title="Delete"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a>';
             html += '</div></td>';
             html += '</tr>';
         } );
